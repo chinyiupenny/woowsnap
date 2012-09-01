ff86a70efd56bd3f12934aab63313813197329b5
chinyiupenny <chinyiu.penny@gmail.com>
2012-09-02 01:42:59 +0200

    Add S3 storage 3

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,9 @@ module JqueryUpload
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    
+    
+    config.assets.precompile += ['admin.js']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
