17bda30694b4df7180d25a4c3b01d5ffbb32d570
chinyiupenny <chinyiu.penny@gmail.com>
2012-09-02 00:44:49 +0200

    Add S3 storage

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'haml-rails'
 gem 'simple_form'
 
 gem 'paperclip'
-
+gem 'aws-s3'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
 class Photo < ActiveRecord::Base
   attr_accessible :caption, :file
-  has_attached_file :file, styles: {thumb: "100x100#", medium: "350x270>"}
-
+  has_attached_file :file, 
+                    :styles => {thumb: "100x100#", medium: "350x270>"}
+                    :storage => :s3,
+                    :s3_credentials => S3_CREDENTIALS
   belongs_to :product
 end
