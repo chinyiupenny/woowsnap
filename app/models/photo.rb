e5198874d858e6a055b9bc1b65de82295a5f3780
chinyiupenny <chinyiu.penny@gmail.com>
2012-09-02 01:28:03 +0200

    Add S3 storage 2

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   attr_accessible :caption, :file
   has_attached_file :file, 
-                    :styles => {thumb: "100x100#", medium: "350x270>"}
+                    :styles => {thumb: "100x100#", medium: "350x270>"},
                     :storage => :s3,
                     :s3_credentials => S3_CREDENTIALS
   belongs_to :product
